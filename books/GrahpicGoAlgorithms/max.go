a57149e1357e3baff13d2468d635cea1123d79ab
cliquebenito <skrollbrad@mail.ru>
2024-02-01 22:47:18 +0300

    books

---
+++ books/GrahpicGoAlgorithms/max.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	scores := []int{14, 12, 55, 34, 96, 83, 23}
+	fmt.Println(max(scores))
+}
+
+func max(scores []int) []int {
+
+	for i := 0; i < len(scores); i++ {
+		for j := i; j < len(scores); j++ {
+			if scores[i] > scores[j] {
+				scores[j], scores[i] = scores[i], scores[j]
+
+			}
+
+		}
+	}
+	fmt.Println(scores)
+
+	return scores
+
+}
